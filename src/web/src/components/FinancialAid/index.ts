1434626fb6f41fa9a159de310475e5cace460707
mboktor <mboktor@stanbridge.edu>
2025-03-16 02:28:55 -0700

    Add public-samples/portal-2.0-nvgpmg/src/web/src/components/FinancialAid/index.ts

---
+++ src/web/src/components/FinancialAid/index.ts
@@ -0,0 +1,13 @@
+import React from 'react'; // Import React from react
+
+// Internal imports
+import AidApplicationForm from './AidApplicationForm';
+import AidApplicationList from './AidApplicationList';
+import AidDocumentUpload from './AidDocumentUpload';
+
+// Export components
+export {
+  AidApplicationForm, // Export form component for creating and editing financial aid applications
+  AidApplicationList, // Export component for displaying a list of financial aid applications
+  AidDocumentUpload, // Export component for uploading financial aid documents
+};
