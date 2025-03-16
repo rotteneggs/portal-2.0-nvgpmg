d1c630b2e1dc787519066bd5a026e5e6c71b7876
mboktor <mboktor@stanbridge.edu>
2025-03-16 02:28:24 -0700

    Add public-samples/portal-2.0-nvgpmg/src/web/src/components/Common/index.ts

---
+++ src/web/src/components/Common/index.ts
@@ -0,0 +1,102 @@
+// Barrel file that exports all common UI components from the Common directory,
+// providing a centralized import point for reusable components throughout the application.
+
+import Button from './Button';
+import { ButtonProps } from './Button';
+import Card from './Card';
+import { CardProps } from './Card';
+import Checkbox from './Checkbox';
+import DatePicker from './DatePicker';
+import EmptyState from './EmptyState';
+import ErrorBoundary from './ErrorBoundary';
+import FileUploader from './FileUploader';
+import Form from './Form';
+import LoadingSkeleton from './LoadingSkeleton';
+import Modal from './Modal';
+import { ModalProps } from './Modal';
+import Notification from './Notification';
+import ProgressIndicator from './ProgressIndicator';
+import RadioButton from './RadioButton';
+import Select from './Select';
+import StatusBadge from './StatusBadge';
+import Table from './Table';
+import { TableProps, TableColumn } from './Table';
+import Tabs from './Tabs';
+import TextField from './TextField';
+import { TextFieldProps } from './TextField';
+import Breadcrumbs from './Breadcrumbs';
+
+// Export Button component for use throughout the application
+export { Button };
+
+// Export Button props interface for type checking
+export type { ButtonProps };
+
+// Export Card component for use throughout the application
+export { Card };
+
+// Export Card props interface for type checking
+export type { CardProps };
+
+// Export Checkbox component for use throughout the application
+export { Checkbox };
+
+// Export DatePicker component for use throughout the application
+export { DatePicker };
+
+// Export EmptyState component for use throughout the application
+export { EmptyState };
+
+// Export ErrorBoundary component for use throughout the application
+export { ErrorBoundary };
+
+// Export FileUploader component for use throughout the application
+export { FileUploader };
+
+// Export Form component for use throughout the application
+export { Form };
+
+// Export LoadingSkeleton component for use throughout the application
+export { LoadingSkeleton };
+
+// Export Modal component for use throughout the application
+export { Modal };
+
+// Export Modal props interface for type checking
+export type { ModalProps };
+
+// Export Notification component for use throughout the application
+export { Notification };
+
+// Export ProgressIndicator component for use throughout the application
+export { ProgressIndicator };
+
+// Export RadioButton component for use throughout the application
+export { RadioButton };
+
+// Export Select component for use throughout the application
+export { Select };
+
+// Export StatusBadge component for use throughout the application
+export { StatusBadge };
+
+// Export Table component for use throughout the application
+export { Table };
+
+// Export Table props interface for type checking
+export type { TableProps };
+
+// Export TableColumn interface for type checking
+export type { TableColumn };
+
+// Export Tabs component for use throughout the application
+export { Tabs };
+
+// Export TextField component for use throughout the application
+export { TextField };
+
+// Export TextField props interface for type checking
+export type { TextFieldProps };
+
+// Export Breadcrumbs component for use throughout the application
+export { Breadcrumbs };
