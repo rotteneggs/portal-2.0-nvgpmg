1cebacc1620d53754d716015aa8c8239f13925a6
mboktor <mboktor@stanbridge.edu>
2025-03-16 02:26:53 -0700

    Add public-samples/portal-2.0-nvgpmg/src/web/src/components/AIAssistant/index.ts

---
+++ src/web/src/components/AIAssistant/index.ts
@@ -0,0 +1,5 @@
+import Chatbot from './Chatbot';
+import DocumentAnalysisResults from './DocumentAnalysisResults';
+import RecommendationCard from './RecommendationCard';
+
+export { Chatbot, DocumentAnalysisResults, RecommendationCard };
