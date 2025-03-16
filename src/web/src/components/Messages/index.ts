9a203614fad637382590dfd5d8a455c199df3138
mboktor <mboktor@stanbridge.edu>
2025-03-16 02:29:04 -0700

    Add public-samples/portal-2.0-nvgpmg/src/web/src/components/Messages/index.ts

---
+++ src/web/src/components/Messages/index.ts
@@ -0,0 +1,16 @@
+import MessageAttachment from './MessageAttachment';
+import MessageComposer from './MessageComposer';
+import MessageInbox from './MessageInbox';
+import MessageThread from './MessageThread';
+
+export type { MessageAttachmentProps } from './MessageAttachment';
+export type { MessageComposerProps } from './MessageComposer';
+export type { MessageInboxProps } from './MessageInbox';
+export type { MessageThreadProps } from './MessageThread';
+
+export {
+    MessageAttachment,
+    MessageComposer,
+    MessageInbox,
+    MessageThread,
+};
